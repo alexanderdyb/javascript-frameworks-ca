af08e8b717f60f285e48c06977acb301065a6a23
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2023-11-27 21:30:33 +0100

    Fetching data on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,54 @@
 import BackgroundImage from "../../components/BackgroundImage";
 import Section from "../../components/Section";
+import { useEffect, useState } from "react";
+
+const url = "https://api.noroff.dev/api/v1/online-shop;";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+
+        const response = await fetch(url);
+        const json = await response.json();
+        setProducts(json);
+
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+    getProducts();
+    console.log(products);
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading products</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
+
   return (
     <>
       <BackgroundImage />
       <Section>
-        <p>Home</p>
+        <div>
+          {products.map((product) => (
+            <div>
+              <img src={product.imageUrl} />
+              <h2>{product.title}</h2>
+            </div>
+          ))}
+        </div>
       </Section>
     </>
   );
